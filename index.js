bfae84d2b1ad7ef22cd639e63ad1a79974196e9d
LookAtThisUrl <mz257@scarletmail.rutgers.edu>
2017-12-26 20:43:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('s501ri5oeCEFKobdUAuGcjd6MPCATq0c'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
